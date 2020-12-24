40dfc1d76e3866cf58afb7b85aba620e16db7bc4
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-12-25 02:16:44 +0900

    add animation to html element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import React from 'react';
+import InViewMonitor from 'react-inview-monitor';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <InViewMonitor
+          classNameNotInView='vis-hidden'
+          classNameInView='animated fadeInUp'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <header className="animate__animated animate__bounce animate__delay-2s">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+          </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+          </a>
+          </header>
+        </InViewMonitor>
+      </div>
+    )
+  }
 }
 
 export default App;
